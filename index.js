ce75b79d9cc943e3a9ac8aa0794e21259a9b07bd
恩仔 <87007372+enenmia@users.noreply.github.com>
2023-06-03 17:13:15 +0800

    initial commit
    deleted the json and vite file

---
+++ index.js
@@ -0,0 +1,29 @@
+let homeNumEl=document.getElementById("homenum")
+let homeNum=0
+function homeplusone(){
+    homeNumEl.textContent=homeNum+1
+    homeNum+=1
+}
+function homeplustwo(){
+    homeNumEl.textContent=homeNum+2
+    homeNum+=2
+}
+function homeplusthree(){
+    homeNumEl.textContent=homeNum+3
+    homeNum+=3
+}
+
+let guestNumEl=document.getElementById("guestnum")
+let guestNum=0
+function guestplusone(){
+    guestNumEl.textContent=guestNum+1
+    guestNum+=1
+}
+function guestplustwo(){
+    guestNumEl.textContent=guestNum+2
+    guestNum+=2
+}
+function guestplusthree(){
+    guestNumEl.textContent=guestNum+3
+    guestNum+=3
+}
